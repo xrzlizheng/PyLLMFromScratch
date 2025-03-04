55949c9238e6ccf8e43bcd12aa9470d777cf9b49
Tianyi (Alex) Qiu <qiutianyi.qty@gmail.com>
2025-03-04 11:23:55 +0800

    chore: reduce model depth

--- config/config.py
+++ config/config.py
@@ -1,11 +1,11 @@
 # --- Configuration ---
 
-# Define vocabulary size and transformer configuration (3 Billion)
+# Define vocabulary size and transformer configuration (0.8 Billion)
 VOCAB_SIZE = 50304          # Number of unique tokens in the vocabulary
 CONTEXT_LENGTH = 512        # Maximum sequence length for the model
 N_EMBED = 2048              # Dimension of the embedding space
 N_HEAD = 16                 # Number of attention heads in each transformer block
-N_BLOCKS = 64               # Number of transformer blocks in the model
+N_BLOCKS = 16               # Number of transformer blocks in the model
 # CONTEXT_LENGTH = 16        # Maximum sequence length for the model
 # N_EMBED = 32              # Dimension of the embedding space
 # N_HEAD = 8                 # Number of attention heads in each transformer block
