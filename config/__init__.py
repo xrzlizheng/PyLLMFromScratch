73c073bf4424e098d238f2f67f15aa28bf63f3ec
Tianyi (Alex) Qiu <qiutianyi.qty@gmail.com>
2025-03-04 10:51:15 +0800

    chore: adding __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+# This file makes the 'src.models' directory a Python package.
+from .mlp import MLP
+from .attention import MultiHeadAttn
+from .transformer_block import TransformerBlock
+from .transformer import Transformer
