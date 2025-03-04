2d0c8b18644e03715cac5ba5759e34cb6f4ad553
Tianyi (Alex) Qiu <qiutianyi.qty@gmail.com>
2025-03-04 08:40:50 +0800

    feat: re-implement network architecture

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 # This file makes the 'src.models' directory a Python package.
 from .mlp import MLP
-from .attention import Head, MultiHeadAttention
-from .transformer_block import Block
+from .attention import MultiHeadAttn
+from .transformer_block import TransformerBlock
 from .transformer import Transformer
