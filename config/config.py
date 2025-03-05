8e1fbb78a29a6128c5cf324ffe18bc06d0cb8779
Fareed Khan <whereismymind557@gmail.com>
2025-03-05 16:28:29 +0500

    Revert "Fix attention scaling factor"

--- config/config.py
+++ config/config.py
@@ -1,15 +1,11 @@
 # --- Configuration ---
 
-# Define vocabulary size and transformer configuration (0.8 Billion)
+# Define vocabulary size and transformer configuration (3 Billion)
 VOCAB_SIZE = 50304          # Number of unique tokens in the vocabulary
 CONTEXT_LENGTH = 512        # Maximum sequence length for the model
 N_EMBED = 2048              # Dimension of the embedding space
 N_HEAD = 16                 # Number of attention heads in each transformer block
-N_BLOCKS = 16               # Number of transformer blocks in the model
-# CONTEXT_LENGTH = 16        # Maximum sequence length for the model
-# N_EMBED = 32              # Dimension of the embedding space
-# N_HEAD = 8                 # Number of attention heads in each transformer block
-# N_BLOCKS = 1               # Number of transformer blocks in the model
+N_BLOCKS = 64               # Number of transformer blocks in the model
 
 # Paths to training and development datasets
 TRAIN_PATH = "data/train/pile_train.h5"  # File path for the training dataset
@@ -22,14 +18,12 @@ T_TRAIN_STEPS = 200000     # Total number of training steps
 T_EVAL_STEPS = 1000        # Frequency (in steps) to perform evaluation
 T_EVAL_ITERS = 250         # Number of iterations to evaluate the model
 T_LR_DECAY_STEP = 50000    # Step at which to decay the learning rate
-T_LR = 5e-5                # Initial learning rate for training
-T_LR_DECAYED = 5e-6        # Learning rate after decay
-T_LR_DECAY_RATE = 0.95
+T_LR = 5e-4                # Initial learning rate for training
+T_LR_DECAYED = 5e-5        # Learning rate after decay
 T_OUT_PATH = "models/transformer_B.pt"  # Path to save the trained model
 
 # Device configuration
 DEVICE = 'cuda'
-# DEVICE = 'cpu'
 
 # Store all configurations in a dictionary for easy access and modification
 default_config = {
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 # This file makes the 'src.models' directory a Python package.
 from .mlp import MLP
-from .attention import MultiHeadAttn
-from .transformer_block import TransformerBlock
+from .attention import Head, MultiHeadAttention
+from .transformer_block import Block
 from .transformer import Transformer
